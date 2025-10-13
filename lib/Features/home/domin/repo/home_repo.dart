633086fcbb5b149c76c81c95a9154bee82d951bd
zeyad ahmed nour <zezoflutter2025@gmail.com>
2025-10-13 15:57:18 +0300

    used Either class in home repos

--- lib/Features/home/domin/repo/home_repo.dart
+++ lib/Features/home/domin/repo/home_repo.dart
@@ -1,6 +1,8 @@
 import 'package:bokly_app/Features/home/domin/entitys/book_entity.dart';
+import 'package:bokly_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> featchFeatureBooks();
-  Future<List<BookEntity>> featchNewsBooks();
+  Future<Either<Failure,List<BookEntity>>> featchFeatureBooks();
+  Future<Either<Failure,List<BookEntity>>> featchNewsBooks();
 }
