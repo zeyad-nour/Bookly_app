2200fd1f07bf71b237edcb46d2a6bc1209be37a5
zeyad ahmed nour <zezoflutter2025@gmail.com>
2025-10-17 20:21:21 +0300

    Implement remote Fetch featured book

--- lib/Features/home/data/data_sources/home_remote_data_source.dart
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -1,15 +1,26 @@
+import 'package:bokly_app/Features/home/data/Models/book_model/book_model.dart';
 import 'package:bokly_app/Features/home/domin/entitys/book_entity.dart';
+import 'package:bokly_app/core/utils/api_serves.dart';
 
 abstract class HomeRemoteDataSource {
-  Future <List<BookEntity>>fetchFeaturedBooks();
-  Future <List<BookEntity>>fetchNewestBooks();
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
 }
 
-class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  final ApiService apiService;
+
+  HomeRemoteDataSourceImpl(this.apiService);
   @override
-  Future<List<BookEntity>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var data = await apiService.get(
+      endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming',
+    );
+    List<BookEntity> books = [];
+    for (var bookMap in data['items']) {
+      books.add(BookModel.fromJson(bookMap));
+    }
+    return books;
   }
 
   @override
